23a9e56f4f58e05bd0425a4ecc8352e706eeae72
lukas kotatko <lukas.kotatko@heureka.cz>
2024-05-18 17:24:16 +0200

    1. chapter done

--- src/1_pycode/main.py
+++ src/1_pycode/main.py
@@ -1,4 +1,4 @@
-# this is an example of using two chains combined together. 
+# this is an example of using two chains combined together.
 # forst chain produces some sample code and send it to second chain which should generate tests for it
 
 from langchain.chains.llm import LLMChain
@@ -19,29 +19,30 @@ args = parser.parse_args()
 
 llm = OpenAI()
 
+# 1. chain
 code_prompt = PromptTemplate(
     input_variables=["task", "language"],
     template="Write a very short {language} function that will {task}",
 )
-
-test_prompt = PromptTemplate(
-    input_variables=["language", "code"],
-    template="Write a test for he following {language} code:<n{code}"
-)
-
 code_chain = LLMChain(
     llm=llm,
     prompt=code_prompt,
     output_key="code"
 )
 
+# 2. chain
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for he following {language} code:<n{code}"
+)
 test_chain = LLMChain(
     llm=llm,
     prompt=test_prompt,
     output_key="test",
 )
 
-# main chain to bind the output of 1. as input to 2., generating answer
+# here is where the magic happens - I need to wire the chains together
+# because it is a sequential action
 chain = SequentialChain(
     chains=[code_chain, test_chain],
     input_variables=["task", "language"],
